0cd7762a7dc6d342518810292147d4f290256040
Siddhartha Bhaumik <38511681+sidbhaumik@users.noreply.github.com>
2022-04-04 00:05:36 -0400

    Add files via upload

--- completed/assignment03/completed_assignment_0302_BhaumikSiddhartha.R
+++ completed/assignment03/completed_assignment_0302_BhaumikSiddhartha.R
@@ -38,9 +38,28 @@ hist.HSDegree <- ggplot(survey_df, aes(HSDegree))  + geom_histogram(aes(y = ..de
 # Display Histogram
 print(hist.HSDegree)
 
+
+#Answer the following questions based on the Histogram produced:
+#  Based on what you see in this histogram, is the data distribution unimodal?
+# Answer: Yes, it's unimodel since it has one clear peak.The values increase at first, rising to a single peak after which it then decrease. 
+
+#  Is it approximately symmetrical?
+# Answer: No, it's not symmetrical since the data is skewed towards left.
+
+#  Is it approximately bell-shaped?
+# Answer: No, its not bell-shaped.
+    
+# Is it approximately normal?
+# Answer: It's not normal.
+
+#  If not normal, is the distribution skewed? If so, in which direction?
+# Answer: The data distribution is skewed towards the left.
+
+#  Include a normal curve to the Histogram that you plotted.
 # Adding another layer to the above chart, the normal curve
 hist.HSDegree + stat_function(fun = dnorm, args = list(mean = mean(survey_df$HSDegree, na.rm = TRUE), sd = sd(survey_df$HSDegree, na.rm = TRUE)), colour = "black", size = 1)
-
+  
+# Explain whether a normal distribution can accurately be used as a model for this data.
 
 # Probability Plot of the HSDegree variable
 # creating random data
@@ -50,4 +69,16 @@ random_values = rnorm(survey_df$HSDegree, mean = 90, sd = 50)
 # And adding line with proper properties
 ggplot(mapping = aes(sample = random_values)) + stat_qq_point(size = 2,color = "red") + stat_qq_line(color="green") + labs(title = "HSDegree Probability Plot", x = "Distribution based on HSdegree", y = "Density")
 
+# Answer the following questions based on the Probability Plot:
+#  Based on what you see in this probability plot, is the distribution approximately normal? Explain how you know.
+# Answer: Yes, it's approximately normal since it resides in a shape like straight line.
 
+# If not normal, is the distribution skewed? If so, in which direction? Explain how you know.
+# Answer: No, most of the data points resides in a straight line. So, its not skewed.
+
+# Now that you have looked at this data visually for normality, you will now quantify normality with numbers using the stat.desc() function.
+stat.desc(survey_df)
+
+# In several sentences provide an explanation of the result produced for skew, kurtosis, and z-scores.
+# Answer: The skewness, kurtosis and z-scores in the result set is due to the sample population with uneven distribution between different US states and counties. 
+# Like I see lot of counties covered for California, New Jersey and New York but that's not the case for rest of the states. 
